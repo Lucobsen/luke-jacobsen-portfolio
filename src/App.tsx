81d3135ddf074f66c49c1d2a05889aebb8b2a823
Luke Jacobsen <ljacobsen@brightwork.com>
2022-02-08 18:15:00 +0100

    added clicks to buttons

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,39 @@
-import React from "react";
+import React, { useState } from "react";
 import luke from "./luke-guitar.svg";
 import "./app.scss";
-import { ButtonList } from "./components/button-list/button-list.component";
+import { ButtonList, Skill } from "./components/button-list/button-list.component";
 
-const skillList: string[] = ["developer", "musician", "dungeon-master"];
+const skillList: Skill[] = [
+  {
+    key: "developer",
+    value: "Developer",
+  },
+  {
+    key: "musician",
+    value: "Musician",
+  },
+  {
+    key: "master",
+    value: "Dungeon Master",
+  },
+];
 
 export const App = () => {
+  const [skills, setSkills] = useState<Skill[]>(skillList);
+  const [selectedSkill, setSelectedSkill] = useState<Skill | null>(null);
+
+  const handleClick = (skill: Skill) => {
+    setSelectedSkill(skill);
+  };
+
   return (
     <div className="app-container">
       <header className="app-header">
         <img src={luke} className="app-logo" alt="luke" />
 
         <p style={{ marginBottom: 0 }}>Luke Jacobsen</p>
-        <p style={{ marginTop: 0 }}>Developer, Musician, Master</p>
 
-        <ButtonList skills={skillList} />
+        <ButtonList skills={skills} onButtonClick={handleClick} />
       </header>
     </div>
   );
